7bfdb3cb6353bffc7796c21a911848c9cddbbfb6
Konrad Stobiecki <konrad.stobiecki@gmail.com>
2020-12-10 03:34:43 +0100

    geolocation

--- src/app/dimensions/dimensions.component.ts
+++ src/app/dimensions/dimensions.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { screen, ScreenMetrics } from "tns-core-modules/platform";
+import * as geolocation from "nativescript-geolocation";
+import { Accuracy } from "tns-core-modules/ui/enums";
+import { BehaviorSubject, ReplaySubject } from 'rxjs';
 
 @Component({
     selector: 'ns-dimensions',
@@ -10,16 +13,28 @@ export class DimensionsComponent implements OnInit {
 
     mainScreen: ScreenMetrics = screen.mainScreen;
 
+    geoLocation$: ReplaySubject<geolocation.Location>;
+    geoText$: BehaviorSubject<string>;
+
     testText: string = JSON.stringify({
         scale: screen.mainScreen.scale,
         widthPixels: screen.mainScreen.widthPixels,
         heightPixels: screen.mainScreen.heightPixels,
         widthDIPs: screen.mainScreen.widthDIPs,
         heightDIPs: screen.mainScreen.heightDIPs,
-    })
+    });
 
-    constructor() { }
+    constructor() {
+        this.geoText$ = new BehaviorSubject('N/A');
+        this.geoLocation$ = new ReplaySubject();
+    }
 
     ngOnInit(): void {
+        geolocation.enableLocationRequest().then(() => {
+            geolocation.getCurrentLocation({ desiredAccuracy: Accuracy.high, maximumAge: 5000, timeout: 20000 }).then(location => {
+                this.geoText$.next(JSON.stringify(location));
+                this.geoLocation$.next({ ...location });
+            });
+        });
     }
 }
